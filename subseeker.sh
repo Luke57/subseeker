e13f2f8cee657e8b81271d7d42f595005fe90b36
Luke57 <t.m.kluter@st.hanze.nl>
2023-09-06 19:59:05 +0200

    Update subseeker.sh
    Update output info

--- subseeker.sh
+++ subseeker.sh
@@ -18,16 +18,20 @@ DOMAIN=$1
 OUTPUT_FILE="$DOMAIN-subs.txt"
 
 # Run subfinder and append output to the output file
+printf "Running Subfinder..\n"
 subfinder -all -d "$DOMAIN" -silent >> "$OUTPUT_FILE"
 
 # Run findomain and append output to the output file
+printf "Running Findomain..\n"
 findomain -t "$DOMAIN" --quiet >> "$OUTPUT_FILE"
 
 # Run assetfinder and append output to the output file
+printf "Running Assetfinder..\n"
 assetfinder --subs-only "$DOMAIN" >> "$OUTPUT_FILE"
 
 # Run amass and append output to the output file
 # Amass is currently a bit broken so very dirty workaround.
+printf "Running Amass..\n"
 amass enum -silent -passive -d "$DOMAIN"
 amass db -names -d "$DOMAIN" >> "$OUTPUT_FILE"
 
